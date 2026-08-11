0ebd0803d2b7f649515a84d5a0fc508aebd45fd7
Alvin Djunaedi <djunaedialvin@gmail.com>
2026-08-11 18:58:10 +0700

    Add Country, Currency,and Month of Travel

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,9 @@
 # Now use them
 def print_trip_summary(
     destination,
+    country,
+    currency,
+    month_of_travel,
     days,
     budget,
     travel_style,
@@ -19,15 +22,18 @@ def print_trip_summary(
     print("==========================")
     print("KelanaAI")
     print("==========================")
-    print(f"Destination : {destination}")
-    print(f"Days         : {days}")
-    print(f"Budget       : {budget}")
-    print(f"Style        : {travel_style}")
-    print(f"Hotel Cost   : {hotel_cost}")
-    print(f"Food Cost    : {food_cost}")
-    print(f"Transport    : {transportation_cost}")
-    print(f"Misc Cost    : {miscellaneous_cost}")
-    print(f"Total Cost   : {total_estimated_cost}")
+    print(f"Destination     : {destination}")
+    print(f"Country         : {country}")
+    print(f"Currency        : {currency}")
+    print(f"Month of Travel : {month_of_travel}")
+    print(f"Days            : {days}")
+    print(f"Budget          : {budget}")
+    print(f"Style           : {travel_style}")
+    print(f"Hotel Cost      : {hotel_cost}")
+    print(f"Food Cost       : {food_cost}")
+    print(f"Transport       : {transportation_cost}")
+    print(f"Misc Cost       : {miscellaneous_cost}")
+    print(f"Total Cost      : {total_estimated_cost}")
 
     if total_estimated_cost > budget:
         print("⚠ Budget exceeded.")
@@ -35,5 +41,5 @@ def print_trip_summary(
     print()
 
 # Call it with any trip
-print_trip_summary("Japan", 5, 1500, "Family", 900, 300, 250, 100)
-print_trip_summary("Bali", 3, 800, "Backpacker", 300, 150, 100, 75)
+print_trip_summary("Tokyo", "Japan", "USD", 2,  5, 7500, "Family", 900, 300, 250, 100)
+print_trip_summary("Bali", "Indonesia", "USD", 1, 3, 5800, "Backpacker", 300, 150, 100, 75)
